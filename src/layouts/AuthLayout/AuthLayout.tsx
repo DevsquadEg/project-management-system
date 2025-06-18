906cb0fb2eec79e3907f22bc2d847752e1211a89
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-06-18 19:23:26 +0300

    fix auth layout ui

--- src/layouts/AuthLayout/AuthLayout.tsx
+++ src/layouts/AuthLayout/AuthLayout.tsx
@@ -4,12 +4,12 @@ export default function AuthLayout() {
     return (
         <>
             <div className="auth-container d-flex justify-content-center align-items-center w-100 vh-100 overflow-y-auto">
-                <div className="container-fluid">
+                <div className="container-fluid d-flex flex-column justify-content-center align-items-center ">
                     <div className="w-100 py-5 d-flex justify-content-center">
                         <img className="" src="/pmsLogo.svg" alt="pmslogo" />
                     </div>
-                    <div className="row justify-content-center align-items-center">
-                        <div className="col-md-10 col-lg-8 px-sm-4 px-md-5 p-5 auth-area rounded-4">
+                    <div className="justify-content-center align-items-center">
+                        <div className="px-sm-4 px-md-5 p-5 auth-area rounded-4 mb-3">
                             <Outlet />
                         </div>
                     </div>
