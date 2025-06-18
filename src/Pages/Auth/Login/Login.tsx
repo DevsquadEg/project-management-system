275f00e6a1d4697b935ffc2f63b6e4579d7f38c5
Muhammed-Mahrous-Saleh <ai526841397@gmail.com>
2025-06-18 17:40:13 +0300

    some fixes in authlayout pages

--- src/Pages/Auth/Login/Login.tsx
+++ src/Pages/Auth/Login/Login.tsx
@@ -1,10 +1,9 @@
-
 import { useForm } from "react-hook-form";
-import { useAuth } from "../../../store/AuthContext/AuthContext";
+import { useAuth } from "@/store/AuthContext/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
 import { useState } from "react";
-import type { FormLoginProps } from "../../../interfaces/interfaces.tsx";
+import type { FormLoginProps } from "@/interfaces/interfaces.tsx";
 import { isAxiosError } from "axios";
 // import {
 //   EMAIL_VALIDATION,
@@ -15,141 +14,165 @@ import { USERS_URL } from "@/service/api.ts";
 import validation from "@/service/validation.ts";
 
 export default function Login() {
-  const navigate = useNavigate();
-  const { saveLoginData } = useAuth()!;
-  const [isPassVisible, setIsPassVisible] = useState(false); // eye flash old password
-  const {
-    register,
-    handleSubmit,
-    formState: { errors, isSubmitting },
-  } = useForm<FormLoginProps>({ mode: "onChange" });
+    const navigate = useNavigate();
+    const { saveLoginData } = useAuth();
+    const [isPassVisible, setIsPassVisible] = useState(false); // eye flash old password
+    const {
+        register,
+        handleSubmit,
+        formState: { errors, isSubmitting },
+    } = useForm<FormLoginProps>({ mode: "onChange" });
 
-  // =========== submit login ========
-  const onSubmit = async (data: FormLoginProps) => {
-    try {
-      const response = await axiosInstance.post(USERS_URL.LOGIN, data);
-      localStorage.setItem("token", response.data.token);
-      await saveLoginData();
-      toast.success("Login success!");
-      navigate("/dashboard", { replace: true });
-    } catch (error) {
-      if (isAxiosError(error))
-        toast.error(error?.response?.data?.message || "Something went wrong");
-    }
-  };
-  return (
-    <>
-      <form onSubmit={handleSubmit(onSubmit)} className="text-start">
-        <div className="d-flex flex-column gap-1 mb-5 ">
-          <small className="text-white">welcome to PMS</small>
-          <h2 className="section-title">Login</h2>
-        </div>
+    // =========== submit login ========
+    const onSubmit = async (data: FormLoginProps) => {
+        try {
+            const response = await axiosInstance.post(USERS_URL.LOGIN, data);
+            localStorage.setItem("token", response.data.token);
+            await saveLoginData();
+            toast.success("Login success!");
+            navigate("/dashboard", { replace: true });
+        } catch (error) {
+            if (isAxiosError(error))
+                toast.error(
+                    error?.response?.data?.message || "Something went wrong"
+                );
+        }
+    };
+    return (
+        <>
+            <form onSubmit={handleSubmit(onSubmit)} className="text-start">
+                <div className="d-flex flex-column gap-1 mb-5 ">
+                    <small className="text-white">welcome to PMS</small>
+                    <h2 className="section-title">Login</h2>
+                </div>
 
-        {/* E-mail */}
-        <div className="mb-3">
-          <label htmlFor="email" className="form-label text-warning fw-normal">
-            E-mail
-          </label>
+                {/* E-mail */}
+                <div className="mb-3">
+                    <label
+                        htmlFor="email"
+                        className="form-label text-warning fw-normal"
+                    >
+                        E-mail
+                    </label>
 
-          <div className="border-bottom d-flex align-items-center pb-1">
-            <div className="input-group">
-              <input
-                id="email"
-                type="email"
-                placeholder="Enter your E-mail"
-                {...register("email", {
-                  required: "Email is required",
-                  pattern: {
-                    value: validation.EMAIL_VALIDATION,
-                    message: "Email Must Be Valid",
-                  },
-                })}
-                className="form-control custom-input"
-              />
-            </div>
-          </div>
+                    <div className="border-bottom d-flex align-items-center pb-1">
+                        <div className="input-group">
+                            <input
+                                id="email"
+                                type="email"
+                                placeholder="Enter your E-mail"
+                                {...register("email", {
+                                    required: "Email is required",
+                                    pattern: {
+                                        value: validation.EMAIL_VALIDATION,
+                                        message: "Email Must Be Valid",
+                                    },
+                                })}
+                                className="form-control custom-input"
+                            />
+                        </div>
+                    </div>
 
-          {errors.email && (
-            <p className="text-white" role="alert" style={{ fontSize: 12 }}>
-              {errors.email.message}
-            </p>
-          )}
-        </div>
+                    {errors.email && (
+                        <p
+                            className="text-white"
+                            role="alert"
+                            style={{ fontSize: 12 }}
+                        >
+                            {errors.email.message}
+                        </p>
+                    )}
+                </div>
 
-        {/* Password */}
-        <div className="mb-3">
-          <label
-            htmlFor="password"
-            className="form-label text-warning fw-normal"
-          >
-            Password
-          </label>
+                {/* Password */}
+                <div className="mb-3">
+                    <label
+                        htmlFor="password"
+                        className="form-label text-warning fw-normal"
+                    >
+                        Password
+                    </label>
 
-          <div className="border-bottom d-flex align-items-center pb-1">
-            <div className="input-group">
-              <input
-                id="password"
-                type={isPassVisible ? "text" : "password"}
-                placeholder="Password"
-                {...register("password", {
-                  required: "Password is required",
-                  pattern: {
-                    value: validation.PASSWORD_VALIDATION,
-                    message:
-                      "Minimum 8 chars, with upper/lowercase, number, and special character",
-                  },
-                })}
-                className="form-control custom-input "
-              />
-              <button
-                type="button"
-                onClick={() => setIsPassVisible((prev) => !prev)}
-                onMouseDown={(e) => e.preventDefault()}
-                onMouseUp={(e) => e.preventDefault} // to prevent the feature of unfocus when i click on the icon
-                className="input-group-text btnSlash"
-                id="addon-wrapping"
-              >
-                <i
-                  className={`fa-regular ${
-                    isPassVisible ? "fa-eye " : "fa-eye-slash"
-                  }`}
-                ></i>
-              </button>
-            </div>
-          </div>
+                    <div className="border-bottom d-flex align-items-center pb-1">
+                        <div className="input-group">
+                            <input
+                                id="password"
+                                type={isPassVisible ? "text" : "password"}
+                                placeholder="Password"
+                                {...register("password", {
+                                    required: "Password is required",
+                                    pattern: {
+                                        value: validation.PASSWORD_VALIDATION,
+                                        message:
+                                            "Minimum 8 chars, with upper/lowercase, number, and special character",
+                                    },
+                                })}
+                                className="form-control custom-input "
+                            />
+                            <button
+                                type="button"
+                                onClick={() =>
+                                    setIsPassVisible((prev) => !prev)
+                                }
+                                onMouseDown={(e) => e.preventDefault()}
+                                onMouseUp={(e) => e.preventDefault} // to prevent the feature of unfocus when i click on the icon
+                                className="input-group-text btnSlash"
+                                id="addon-wrapping"
+                            >
+                                <i
+                                    className={`fa-regular ${
+                                        isPassVisible
+                                            ? "fa-eye "
+                                            : "fa-eye-slash"
+                                    }`}
+                                ></i>
+                            </button>
+                        </div>
+                    </div>
 
-          {errors.password && (
-            <p className="text-white" role="alert" style={{ fontSize: 12 }}>
-              {errors.password.message}
-            </p>
-          )}
-        </div>
+                    {errors.password && (
+                        <p
+                            className="text-white"
+                            role="alert"
+                            style={{ fontSize: 12 }}
+                        >
+                            {errors.password.message}
+                        </p>
+                    )}
+                </div>
 
-        <div className="links d-flex justify-content-between my-4  ">
-          <Link
-            className=" text-white text-decoration-none fw-light"
-            to="/register"
-          >
-            Register Now?
-          </Link>
-          <Link
-            className="text-white text-decoration-none fw-light "
-            to="/forget-password"
-          >
-            Forgot Password?
-          </Link>
-        </div>
-        {/* Submit */}
-        <div className="d-grid">
-          <button
-            type="submit"
-            className="btn custom-btn btn-lg "
-            disabled={isSubmitting}
-          >
-            Login
-          </button>
-        </div>
-      </form>
-    </>
-  );
+                <div className="links d-flex justify-content-between my-4  ">
+                    <Link
+                        className=" text-white text-decoration-none fw-light"
+                        to="/register"
+                    >
+                        Register Now?
+                    </Link>
+                    <Link
+                        className="text-white text-decoration-none fw-light "
+                        to="/forget-password"
+                    >
+                        Forgot Password?
+                    </Link>
+                </div>
+                {/* Submit */}
+                <div className="d-grid">
+                    <button
+                        type="submit"
+                        className="btn custom-btn btn-lg "
+                        disabled={isSubmitting}
+                    >
+                        {(isSubmitting && (
+                            <span
+                                className="spinner-border spinner-border-sm"
+                                role="status"
+                                aria-hidden="true"
+                            ></span>
+                        )) ||
+                            "Login"}
+                    </button>
+                </div>
+            </form>
+        </>
+    );
 }
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,91 +1,99 @@
 import { useNavigate } from "react-router-dom";
-import { useAuth } from "../../store/AuthContext/AuthContext";
-
+import { useAuth } from "@/store/AuthContext/AuthContext";
 
 export default function Navbar() {
+    const navigate = useNavigate();
+    const { saveLoginData, loginData }: any = useAuth();
 
-    const navigate =useNavigate();
-    const {saveLoginData,loginData}: any = useAuth();
+    // =========== LogOut ========
+    const logOut = () => {
+        localStorage.removeItem("token");
+        saveLoginData("");
+        navigate("/login");
+    };
 
+    return (
+        <>
+            <div className="d-flex justify-content-between align-items-center py-3 mx-4 ">
+                <div>
+                    <img src="/navLogo.svg" className="img-fluid " alt="PMS" />
+                </div>
+                <div className="d-flex align-items-center justify-content-evenly gap-4 px-2">
+                    {/* <!-- Notification icon --> */}
+                    <div className="position-relative">
+                        <i
+                            style={{ color: "#EF9B28" }}
+                            className="fa-solid fa-bell fs-4 text-warning"
+                        ></i>
+                        <span
+                            className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
+                            style={{
+                                fontSize: "0.6rem",
+                            }}
+                        >
+                            1
+                        </span>
+                    </div>
+                    <div
+                        className="border-start mx-2"
+                        style={{
+                            height: "30px",
+                            borderColor: "#eee",
+                            opacity: 0.5,
+                        }}
+                    ></div>
+                    {/* <!-- User avatar --> */}
+                    <div className="d-flex gap-3">
+                        <img
+                            src="https://randomuser.me/api/portraits/men/75.jpg"
+                            alt="user"
+                            className="rounded-circle"
+                            width="40"
+                            height="40"
+                        />
+                        <div className="d-flex flex-column">
+                            <span>{loginData?.userName}</span>
+                            <small className="text-muted">
+                                {loginData?.userEmail}
+                            </small>
+                        </div>
+                    </div>
 
-  // =========== LogOut ========
-    const logOut = () =>{
-
-      localStorage.removeItem("token");
-      saveLoginData("");
-      navigate('/login');
-      }
-
-
-   
-
-
-  return (
-    <>
-      <div className="d-flex justify-content-between align-items-center py-3 mx-4 ">
-        <div>
-          <img src="/public/navLogo.svg" className="img-fluid " alt="PMS" />
-        </div>
-        <div className="d-flex align-items-center justify-content-evenly gap-4 px-2">
-          {/* <!-- Notification icon --> */}
-          <div className="position-relative">
-            <i
-              style={{ color: "#EF9B28" }}
-              className="fa-solid fa-bell fs-4 text-warning"
-            ></i>
-            <span
-              className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-              style={{
-                fontSize: "0.6rem",
-              }}
-            >
-              1
-            </span>
-          </div>
-          <div
-            className="border-start mx-2"
-            style={{ height: "30px", borderColor: "#eee", opacity: 0.5 }}
-          ></div>
-          {/* <!-- User avatar --> */}
-          <div className="d-flex gap-3">
-            <img
-              src="https://randomuser.me/api/portraits/men/75.jpg"
-              alt="user"
-              className="rounded-circle"
-              width="40"
-              height="40"
-            />
-            <div className="d-flex flex-column">
-              <span>{loginData?.userName}</span>
-              <small className="text-muted">{loginData?.userEmail}</small>
+                    {/* <!-- Arrow --> */}
+                    <div className="dropdown ">
+                        <button
+                            className="btn border-0"
+                            type="button"
+                            data-bs-toggle="dropdown"
+                            aria-expanded="false"
+                        >
+                            <i className="fa-solid fa-angle-down"></i>
+                        </button>
+                        <ul className="dropdown-menu dropdown-menu-end shadow-lg">
+                            <li>
+                                <button
+                                    className="dropdown-item d-flex align-items-center gap-2 text-secondary"
+                                    onClick={() => {
+                                        navigate("/change-password");
+                                    }}
+                                >
+                                    <i className="bi bi-key "></i> Change
+                                    Password
+                                </button>
+                            </li>
+                            <li>
+                                <button
+                                    className="dropdown-item d-flex align-items-center gap-2 text-danger"
+                                    onClick={logOut}
+                                >
+                                    <i className="fa-solid fa-right-from-bracket "></i>{" "}
+                                    Logout
+                                </button>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
             </div>
-          </div>
-
-          {/* <!-- Arrow --> */}
-          <div className="dropdown ">
-            <button
-              className="btn border-0"
-              type="button"
-              data-bs-toggle="dropdown"
-              aria-expanded="false"
-            >
-              <i className="fa-solid fa-angle-down"></i>
-            </button>
-            <ul className="dropdown-menu dropdown-menu-end shadow-lg">
-              <li>
-                <button className="dropdown-item d-flex align-items-center gap-2 text-secondary" onClick={()=>{navigate('/change-password')}}>
-                  <i className="bi bi-key "></i> Change Password
-                </button>
-              </li>
-              <li>
-                <button className="dropdown-item d-flex align-items-center gap-2 text-danger" onClick={logOut}>
-                  <i className="fa-solid fa-right-from-bracket "></i> Logout
-                </button>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 }
